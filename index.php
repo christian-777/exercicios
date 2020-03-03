6a858ab4e6992d50e8f4b305b642c02df7978607
christian-777 <christian.c.g.f777@hotmail.com>
2020-03-03 16:42:50 -0300

    nao fiz aula passada

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>organizaçao atividades web</title>
+	</head>
+	<body>
+		<p><h1>pagina com links dos meus exercicios<h1><p>
+		<hr />
+		<a href="20200219/barbearia_bootstrap"> 20200219 - Barbearia</a>
+	</body>
+</html>
